094b2583254867c629ee10a307fd465a006a2279
rfprod <zoidenmacht@zoho.com>
2018-12-23 23:38:56 +0300

    (fix) removed junky source

--- src/app/elements/orders-table/index.ts
+++
@@ -1,3 +0,0 @@
-export { OrdersTableConfigComponent } from './orders-table-config/orders-table-config.component';
-export { OrdersTableIndexComponent } from './orders-table-index/orders-table-index.component';
-export { OrdersTableWidgetComponent } from './orders-table-widget/orders-table-widget.component';
--- src/app/elements/table/index.ts
+++
@@ -1,2 +0,0 @@
-export { TableConfigComponent } from './table-config/table-config.component';
-export { TableWidgetComponent } from './table-widget/table-widget.component';
