3dcdaed10f24a189d514886972e82029cf97e813
Vadim <rfprod@users.noreply.github.com>
2020-10-14 18:37:56 +0300

    fix(hammer): fix hammjs usage (#41)

--- src/app/modules/material/custom-material.module.ts
+++ src/app/modules/material/custom-material.module.ts
@@ -1,5 +1,3 @@
-import '../../../../node_modules/hammerjs/hammer.js';
-
 import { OverlayModule } from '@angular/cdk/overlay';
 import { ModuleWithProviders, NgModule, Provider } from '@angular/core';
 import {
@@ -50,6 +48,9 @@ import {
   MatTooltipModule,
 } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
+import { HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+import { AppHammerGestureConfig } from './hammerjs-gesture.config';
 
 /**
  * Returns mat tooltip options.
@@ -101,6 +102,7 @@ export const customMaterialModuleProviders: Provider[] = [
     provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS,
     useFactory: matMomentDateAdapterOptionsFactory,
   },
+  { provide: HAMMER_GESTURE_CONFIG, useClass: AppHammerGestureConfig },
   { provide: MAT_DATE_FORMATS, useValue: CUSTOM_DATE_FORMATS },
 ];
 
